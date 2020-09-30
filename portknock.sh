f796025eab7d8ce4d350d54fca0281c36c5adc39
Tobias Dussa <dussa@dfn-cert.de>
2020-09-30 23:40:45 +0200

    Added timed mode.

--- portknock.sh
+++ portknock.sh
@@ -21,6 +21,10 @@ if [ -n "${TCP}" ] || echo $0 | fgrep -qi tcp; then
 	PROTOCOL="tcp"
 fi
 
+if echo $0 | fgrep -qi timed; then
+	TIMED="yes"
+fi
+
 if [ -n "$2" ]; then
 	ROUNDS="$2"
 fi
@@ -39,8 +43,25 @@ while [ ${ROUNDS} -gt 0 ]; do
 	ROUNDS=$((${ROUNDS}-1))
 done
 
+if [ -n "${TIMED}" ]; then
+	TIMEDPORTSTRING=$(echo -n ${PORTSTRING} | sha512sum | cut -c1-120)$(echo -n ${PORTSTRING} | sha512sum | sha512sum | cut -c1-120)
+	TIMEDPORTS=()
+	TIMEDPORTSARRAY=""
+	MINUTE=0
+	while [ -n "${TIMEDPORTSTRING}" ]; do
+		PORT=($((16#$(echo ${TIMEDPORTSTRING} | cut -c1-4))))
+		TIMEDPORTS+=(${PORT})
+		TIMEDPORTSTRING=$(echo ${TIMEDPORTSTRING} | cut -c5-)
+		TIMEDPORTSARRAY="${TIMEDPORTSARRAY}\\\"${MINUTE}\\\"=${PORT};"
+		MINUTE=$((1+${MINUTE}))
+	done
+fi
+
 if [ "${HOST}" == "-d" ]; then
 	echo Port list: ${PORTS[@]}
+	if [ -n "${TIMEDPORTS}" ]; then
+		echo Timed port list: ${TIMEDPORTS[@]}
+	fi
 	exit
 fi
 
@@ -48,11 +69,28 @@ if [ "${HOST}" == "-c" ]; then
 	PREVLIST=""
 	echo "add action=drop chain=input comment=\"Deny access after bogus port knocking attempt\" in-interface-list=WAN log=yes log-prefix=\"Portknock denied from WAN:\" src-address-list=portknock:denied"
 	echo "add action=accept chain=input comment=\"Accept SSH after passed port knocking\" dst-port=22 in-interface-list=WAN log=yes log-prefix=\"SSH access from WAN:\" protocol=tcp src-address-list=portknock:passed"
+	if [ -n "${TIMED}" ]; then
+		LIST="portknock:passed"
+		PREVLIST="portknock:timed"
+		LOG="log=yes log-prefix=\"Portknock passed:\""
+		echo "add action=add-src-to-address-list address-list=${LIST} src-address-list=${PREVLIST} address-list-timeout=5s chain=input comment=\"Port knocking (${PROTOCOL}), timed step previous, proceed\" dst-port=0 in-interface-list=WAN protocol=${PROTOCOL} ${LOG}"
+		echo "add action=drop src-address-list=${PREVLIST} chain=input comment=\"Port knocking (${PROTOCOL}), timed step previous, finish\" dst-port=0 in-interface-list=WAN protocol=${PROTOCOL}"
+		echo "add action=add-src-to-address-list address-list=${LIST} src-address-list=${PREVLIST} address-list-timeout=5s chain=input comment=\"Port knocking (${PROTOCOL}), timed step current, proceed\" dst-port=0 in-interface-list=WAN protocol=${PROTOCOL} ${LOG}"
+		echo "add action=drop src-address-list=${PREVLIST} chain=input comment=\"Port knocking (${PROTOCOL}), timed step current, finish\" dst-port=0 in-interface-list=WAN protocol=${PROTOCOL}"
+		echo "add action=add-src-to-address-list address-list=${LIST} src-address-list=${PREVLIST} address-list-timeout=5s chain=input comment=\"Port knocking (${PROTOCOL}), timed step next, proceed\" dst-port=0 in-interface-list=WAN protocol=${PROTOCOL} ${LOG}"
+		echo "add action=drop src-address-list=${PREVLIST} chain=input comment=\"Port knocking (${PROTOCOL}), timed step next, finish\" dst-port=0 in-interface-list=WAN protocol=${PROTOCOL}"
+		echo "add action=add-src-to-address-list address-list=portknock:denied src-address-list=${PREVLIST} address-list-timeout=5s chain=input comment=\"Port knocking (${PROTOCOL}), timed step, block\" in-interface-list=WAN protocol=${PROTOCOL}"
+		LOG=""
+	fi
 	STEP=${#PORTS[@]}
 	while [ ${STEP} -gt 0 ]; do
 		if [ "${STEP}" -eq "${#PORTS[@]}" ]; then
-			LIST="portknock:passed"
-			LOG="log=yes log-prefix=\"Portknock passed:\""
+			if [ -n "${TIMED}" ]; then
+				LIST="portknock:timed"
+			else
+				LIST="portknock:passed"
+				LOG="log=yes log-prefix=\"Portknock passed:\""
+			fi
 		else
 			LIST="portknock:${STEP}"
 			LOG=""
@@ -73,13 +111,23 @@ if [ "${HOST}" == "-c" ]; then
 		fi
 		STEP=$((${STEP}-1))
 	done
+	if [ -n "${TIMED}" ]; then
+		echo "/system script add comment=\"Portknock Timed Step Updater\" dont-require-permissions=no name=PortknockUpdater owner=admin policy=read,write,test source=\"\\
+local portlist {${TIMEDPORTSARRAY}}\\
+\\nlocal current [:tonum [:pick [/system clock get time] 3 5]]\\
+\\nlocal previous ((\\\$current+59) % 60)\\
+\\nlocal next ((\\\$current+1) % 60)\\
+\\n/ip firewall filter set dst-port=(\\\$portlist->[:tostr \\\$previous]) [/ip firewall filter find comment~\\\"timed step previous\\\"]\\
+\\n/ip firewall filter set dst-port=(\\\$portlist->[:tostr \\\$current]) [/ip firewall filter find comment~\\\"timed step current\\\"]\\
+\\n/ip firewall filter set dst-port=(\\\$portlist->[:tostr \\\$next]) [/ip firewall filter find comment~\\\"timed step next\\\"]\""
+		echo "/system scheduler add comment=\"Portknock Timed Step Updater\" interval=1m name=PortknockUpdater on-event=PortknockUpdater policy=read,write,test start-time=startup"
+	fi
 	exit
 fi
 
 echo -n Portknocking
 for PORT in ${PORTS[@]}; do
 	if [ "${PROTOCOL}" == "tcp" ]; then
-		#nc -zw 1 ${HOST} ${PORT}
 		timeout 0.2 /bin/bash -c "echo '' > /dev/tcp/${HOST}/${PORT}"
 	else
 		echo "" > /dev/udp/${HOST}/${PORT}
@@ -87,4 +135,14 @@ for PORT in ${PORTS[@]}; do
 	fi
 	echo -n .
 done
+if [ -n "${TIMED}" ]; then
+	PORT=${TIMEDPORTS[$(date "+%M")]}
+	if [ "${PROTOCOL}" == "tcp" ]; then
+		timeout 0.2 /bin/bash -c "echo '' > /dev/tcp/${HOST}/${PORT}"
+	else
+		echo "" > /dev/udp/${HOST}/${PORT}
+		sleep 0.2
+	fi
+	echo -n .
+fi
 echo done.
